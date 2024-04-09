171b181aaae77746dfc4f7f5899a79545c8d9f07
lamineniang99 <pisupniang@gmail.com>
2024-04-09 03:15:38 +0000

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from './helpers/auth.guard';
+import { noReturnAuthGuard } from './helpers/no-return-auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    loadChildren : ()=> import('./features/features.module').then(m => m.FeaturesModule),
+    path : "",
+    canActivate : [authGuard]
+  },
+  {
+    loadChildren : () => import('./auth/auth.module').then(m => m.AuthModule),
+    path : "auth" ,
+    canActivate: [noReturnAuthGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+const config: SocketIoConfig = { url: 'http://localhost:4000', options: {} };
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    SocketIoModule.forRoot(config) ,
   ],
   providers: [],
   bootstrap: [AppComponent]
